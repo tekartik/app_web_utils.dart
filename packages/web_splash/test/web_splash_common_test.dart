54a445dffeb735e830f1f2f59d44fc422c309cdf
Alexandre Roux <alex@tekartik.com>
2024-12-01 18:57:03 +0100

    fix test

--- packages/web_splash/test/web_splash_common_test.dart
+++ packages/web_splash/test/web_splash_common_test.dart
@@ -1,10 +1,11 @@
 library;
 
-import 'package:tekartik_web_splash/src/splash_web.dart';
+import 'package:tekartik_web_splash/web_splash.dart';
 import 'package:test/test.dart';
 
 void main() {
   test('show/hide', () async {
+    // no op on non web
     webSplashReady;
     webSplashHide();
   });
